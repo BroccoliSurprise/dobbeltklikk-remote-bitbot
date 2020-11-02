526ddd722f368fa53ff9f61b94562d665f8379f1
BroccoliSurprise <61516592+BroccoliSurprise@users.noreply.github.com>
2020-11-02 22:12:53 +0100

    la til demo-lys

--- main.ts
+++ main.ts
@@ -3,7 +3,19 @@
  * 
  * Kjører på radiokanal 8
  */
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # # # # #
+        . # # # .
+        `)
+    music.playMelody("- G - G - - - - ", 120)
+    bitbot.rotatems(BBRobotDirection.Left, 60, 3000)
+})
 radio.onReceivedString(function (receivedString) {
+    demo = 0
     if (receivedString == "nedA") {
         bitbot.move(BBMotor.Right, BBDirection.Forward, 60)
     } else if (receivedString == "oppA") {
@@ -33,7 +45,15 @@ radio.onReceivedString(function (receivedString) {
     	
     }
 })
+input.onButtonPressed(Button.B, function () {
+    demo = 1
+    bitbot.setLedColor(0xFF0000)
+    for (let indeks = 0; indeks <= 6; indeks++) {
+        bitbot.setPixelColor(indeks * 2, 0x0000FF)
+    }
+})
 let klikkAtoggle = 0
+let demo = 0
 bitbot.select_model(BBModel.XL)
 bitbot.ledBrightness(40)
 radio.setGroup(8)
@@ -41,7 +61,7 @@ let ledTimer = 12
 let tikk = 0
 // Loop uten "pause"
 basic.forever(function () {
-    if (input.runningTime() > tikk + ledTimer) {
+    if (input.runningTime() > tikk + ledTimer && demo == 0) {
         bitbot.ledRotate()
         tikk = input.runningTime()
     }
